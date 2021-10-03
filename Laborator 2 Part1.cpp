06da24ad7e8c3c3e0191d9b3b2df63822fc304a2
livia110 <74725718+livia110@users.noreply.github.com>
2021-10-03 23:29:07 +0300

    Update Laborator 2 Part1.cpp

--- Laborator 2 Part1.cpp
+++ Laborator 2 Part1.cpp
@@ -49,46 +49,9 @@ public: int Aria_Box() {
 }
 };
 
-class  Queue
-{
-public:
-	int *tab;// staocarea elementelor in tabel
-	int nr_elem_Max;//numarul maxim  de elemente
-	int prim_Elem;//primul element al cozii
-	int k; // numarul de elemente din coada
-
-public:
-	Queue() {
-		tab = 0;
-		nr_elem_Max = 0;
-		prim_Elem = 0;
-		k = 0;
-	}
-public:
-	Queue(int *t, int nr_elem, int prim, int k_e) {
-		*tab = *t;
-		nr_elem_Max = nr_elem;
-		prim_Elem = prim;
-		k = k_e;
-	}
-public: void //functia de citire a datelor
-	Read() {
-		cout << "\n\t---Introduceti datele---" << endl;
-		cout << "";
-}
-public: void // functia de afisare a datelor
-	Display() {
-		cout << "\n\t---Afisarea datele---" << endl;
-
-}
-
-};
-
 int main() {
-	queue <int> q;
 	Box b;
 	int n;//numarul de cutii
-	int m;//numarul de elemente in coada
 	cout << "\n\tIntroduceti numarul de elemente:";
 	cin >> n;// numarul de elemete
 	for (int i = 0; i < n; i++)//ciclu de citire/afisare a nr elemente de le tasta
@@ -97,19 +60,5 @@ int main() {
 		b.Display();
 		cout << "\n\tVolumul cutiei: "<<b.Volum_Box();
 		cout << "\n\tAria cutiei: " << b.Aria_Box()<<endl;
-	}
-
-	cout << "\n\n\t---Afisarea cozii---"<<endl;
-	cout << "\n\tIntroduceti numarul de elemente:";
-	cin >> m;
-	for (int i = 0; i < m; i++)
-	{
-		cout << "\tElemetele:"<<i<<endl;
-
-		q.push(i);
-	}
-	cout << "\n\tMarimea cozii:" << q.size() << endl;
-	for (int i = 0; i < m; i++) {
-		q.pop();
 	}
 }
