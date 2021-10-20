6ca291272b9765314b4f75bb2b8d9e81661793f0
livia110 <74725718+livia110@users.noreply.github.com>
2021-10-20 16:37:23 +0300

    Add files via upload

---
+++ main.java
@@ -0,0 +1,14 @@
+package javaapplication6;
+
+
+import javaapplication6.Organization;
+
+public class main {
+    static public void main(String[] args)
+    {
+        Organization org = new Organization();
+
+        System.out.println(org);
+    }
+}
+
